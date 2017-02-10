8d1a5d5ec53cfc9ecf0dc3118c6cf2e382979ead
Kishan Jadav <kishan_jadav@hotmail.com>
2017-02-10 13:35:48 -0800

    update 1.2

--- app/src/main/java/com/kishan/exames/ExamesGoActivity.java
+++ app/src/main/java/com/kishan/exames/ExamesGoActivity.java
@@ -1,37 +1,63 @@
 package com.kishan.exames;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Environment;
+import android.os.Handler;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnDrawListener;
+import com.github.barteksc.pdfviewer.listener.OnErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.sothree.slidinguppanel.ScrollableViewHelper;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import static com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState.COLLAPSED;
+
 
 public class ExamesGoActivity extends AppCompatActivity {
 
-    public PDFView pdfView;
+    public PDFView pdfView, pdfPanel;
+    private SlidingUpPanelLayout slideUp;
+    private TextView pushtext, notice;
+    private LinearLayout lere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_exames_go);
 
         // init the PDFView:
         pdfView = (PDFView) findViewById(R.id.pdfView);
+        pdfPanel = (PDFView) findViewById(R.id.pdfPanel);
+        slideUp = (SlidingUpPanelLayout) findViewById(R.id.activity_exames_go);
+        pushtext = (TextView) findViewById(R.id.dragView);
+        notice = (TextView) findViewById(R.id.tvNotice);
+        lere = (LinearLayout) findViewById(R.id.lere);
 
-        // Get the file choosen/selected from the "MaterialFilePicker_LIBRARY" on ExamesActivity:
+        notice.setVisibility(View.GONE);
+
+
+        // Get the file from "MaterialFilePicker_LIBRARY" to the Main PDF View (pdfView):
         Intent intent = getIntent();
         String filePath = intent.getStringExtra("FILE_PDF");
 
@@ -47,18 +73,98 @@ public class ExamesGoActivity extends AppCompatActivity {
                         .enableDoubletap(true)
                         .defaultPage(0)
                         .enableAnnotationRendering(false)
+                        .onLoad(new OnLoadCompleteListener() {
+                            @Override
+                            public void loadComplete(int nbPages) {
+                                Toast.makeText(ExamesGoActivity.this, "Enunciado aberto.", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .onError(new OnErrorListener() {
+                            @Override
+                            public void onError(Throwable t) {
+                                Toast.makeText(ExamesGoActivity.this, "Ocorreu um erro. Baixe o enunciado novamente.", Toast.LENGTH_SHORT).show();
+                            }
+                        })
                         .load();
-
-                Toast.makeText(ExamesGoActivity.this, "exame aberto.", Toast.LENGTH_SHORT).show();
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
-                Toast.makeText(this, "Whoops, o exame parece estar corrumpido. Tente baixar denovo.", Toast.LENGTH_LONG).show();
             }
 
 
 
+        // Get the file from "MaterialFilePicker_LIBRARY" to the SlideUpPanel PDF View (pdfPanel):
+        Intent intss = getIntent();
+        final String filee = intss.getStringExtra("GUIA_PDF");
 
+
+        File doesTheFileExist = new File(filee);
+
+        if(doesTheFileExist.exists()){
+            Toast.makeText(this, "Guia, acima disponível...", Toast.LENGTH_SHORT).show();
+
+            FileInputStream fileeinspit = null;
+            try {
+                fileeinspit = new FileInputStream(filee);
+
+                pdfPanel.fromStream(fileeinspit)
+                        .enableSwipe(true)
+                        .swipeHorizontal(false)
+                        .enableDoubletap(true)
+                        .defaultPage(0)
+                        .enableAnnotationRendering(false)
+                        .onError(new OnErrorListener() {
+                            @Override
+                            public void onError(Throwable t) {
+                                Toast.makeText(ExamesGoActivity.this, "Ocorreu um erro. Baixe a guia novamente.", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .load();
+
+
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }else if(!doesTheFileExist.exists()){
+            // Gets the layout params that will allow you to resize the layout
+            pushtext.setText("...");
+            notice.setVisibility(View.VISIBLE);
+            pdfPanel.setVisibility(View.GONE);
+            slideUp.setAnchorPoint(0.5f);
+        }
+
+
+    }
+    //Back button action setter:
+    boolean doubleBackToExitPressedOnce = false;
+    @Override
+    public void onBackPressed() {
+
+       if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+
+            String dd = slideUp.getPanelState().toString();
+
+            if (!dd.equals("COLLAPSED")) {
+                slideUp.setPanelState(COLLAPSED);
+                return;
+            }
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Clique novamente para sair", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce=false;
+                }
+            }, 2000);
+
+       /* super.onBackPressed();*/
     }
 
 }
